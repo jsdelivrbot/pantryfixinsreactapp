1171653a03fbc07bf95a9ba9b079f50e9c20e87f
kmgulledge <kmgulledge@gmail.com>
2017-11-14 15:42:53 -0500

    i hate heroku

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,8 @@
+var path = require('path');
+var webpack = require('webpack');
+
 module.exports = {
+  devtool: null,
   entry: [
     './src/index.js'
   ],
@@ -7,10 +11,22 @@ module.exports = {
     publicPath: '/',
     filename: 'bundle.js'
   },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      minimize: true,
+      compress: {
+        warnings: false
+      }
+    }),
+    new webpack.DefinePlugin({
+      'process.env': { NODE_ENV: '"production"' }
+    })
+  ],
   module: {
     loaders: [{
+      test: /\.js$/,
       exclude: /node_modules/,
-      loader: 'babel',
+      loader: 'babel-loader',
       query: {
         presets: ['react', 'es2015', 'stage-1']
       }
